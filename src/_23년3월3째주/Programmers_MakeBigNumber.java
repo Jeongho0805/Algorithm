3c282faf6ecaf3bbe732a97cf69f3a0a73195009
JeonghoPark <pjhpjh0805@naver.com>
2023-03-27 21:55:02 +0900

    2023-03-26 풀이

---
+++ src/_23년3월3째주/Programmers_MakeBigNumber.java
@@ -0,0 +1,30 @@
+package _23년3월3째주;
+
+/**
+ * 링크 - https://school.programmers.co.kr/learn/courses/30/lessons/42883?language=java
+ *
+ */
+public class Programmers_MakeBigNumber {
+    public String solution(String number, int k) {
+
+        StringBuilder answer = new StringBuilder();
+        int extraSpace = number.length() - k;
+        char[] numbers = number.toCharArray();
+
+        for (int i=0; i<numbers.length; i++) {
+            if (extraSpace == 0) {
+                break;
+            }
+            int maxValue = 0;
+            for (int j=i; j<= numbers.length - extraSpace; j++) {
+                if (maxValue < numbers[j] - '0') {
+                    maxValue = numbers[j] - '0';
+                    i = j;
+                }
+            }
+            answer.append(maxValue);
+            extraSpace--;
+        }
+        return answer.toString();
+    }
+}
---
+++ test/Programmers_MakeBigNumberTest.java
@@ -0,0 +1,19 @@
+import _23년3월3째주.Programmers_MakeBigNumber;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Programmers_MakeBigNumberTest {
+
+    private final Programmers_MakeBigNumber problem = new Programmers_MakeBigNumber();
+
+    @Test
+    void test1() {
+        String input = "4177252841";
+        int k = 4;
+        String result = problem.solution(input, k);
+
+        assertEquals( "775841", result);
+    }
+}
