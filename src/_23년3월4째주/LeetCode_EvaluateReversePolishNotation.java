b5e3338bbf975407013ac4a359a3ace919b609c9
JeonghoPark <pjhpjh0805@naver.com>
2023-03-27 22:40:14 +0900

    2023-03-27 풀이
    - LeetCode_ContainsDuplicate
    - LeetCode_EvaluateReversePolishNotation

---
+++ src/_23년3월4째주/LeetCode_EvaluateReversePolishNotation.java
@@ -0,0 +1,35 @@
+package _23년3월4째주;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * 링크 - https://leetcode.com/problems/evaluate-reverse-polish-notation/
+ */
+public class LeetCode_EvaluateReversePolishNotation {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> st = new Stack<>();
+        List<String> operators = Arrays.asList("+", "-", "*", "/");
+        for (String token : tokens) {
+            if (operators.contains(token)) {
+                int num2 = st.pop();
+                int num1 = st.pop();
+                int result = 0;
+                if (token.equals("+")) {
+                    result = num1 + num2;
+                } else if (token.equals("-")) {
+                    result = num1 - num2;
+                } else if (token.equals("*")) {
+                    result = num1 * num2;
+                } else {
+                    result = num1 / num2;
+                }
+                st.push(result);
+                continue;
+            }
+            st.push(Integer.parseInt(token));
+        }
+        return st.pop();
+    }
+}
---
+++ test/LeetCode_ContainsDuplicateTest.java
@@ -0,0 +1,31 @@
+import _23년3월4째주.LeetCode_ContainsDuplicate;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LeetCode_ContainsDuplicateTest {
+
+    private LeetCode_ContainsDuplicate problem = new LeetCode_ContainsDuplicate();
+
+    @Test
+    void test1() {
+        int[] nums = {1,2,3,1};
+        boolean result = problem.containsDuplicate(nums);
+        assertTrue(result);
+    }
+
+    @Test
+    void test2() {
+        int[] nums = {1,2,3,4};
+        boolean result = problem.containsDuplicate(nums);
+        assertFalse(result);
+    }
+
+    @Test
+    void test3() {
+        int[] nums = {1,1,1,3,3,4,3,2,4,2};
+        boolean result = problem.containsDuplicate(nums);
+        assertTrue(result);
+    }
+}
