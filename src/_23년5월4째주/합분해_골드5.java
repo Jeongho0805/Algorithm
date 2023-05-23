f30e10d781e0d58a31a8f6d37b19b1425b79450f
JeonghoPark <pjhpjh0805@naver.com>
2023-05-23 16:40:17 +0900

    bj_2225

---
+++ src/_23년5월4째주/합분해_골드5.java
@@ -0,0 +1,40 @@
+package _23년5월4째주;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * https://www.acmicpc.net/problem/2225
+ */
+public class 합분해_골드5 {
+
+    private static long MOD = 1_000_000_000L;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        long[][] dp = new long[k][n+1];
+
+        for (int i=0; i<=n; i++) {
+            dp[0][i] = 1;
+        }
+
+        for (int i=0; i<k; i++) {
+            dp[i][0] = 1;
+        }
+
+        for (int i=1; i<k; i++) {
+            for (int j=1; j<=n; j++) {
+                dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % MOD;
+            }
+        }
+        System.out.println(dp[k-1][n]);
+    }
+}
