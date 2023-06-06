414fee5e9cd0ff24ab5eac7002d3b5ad8291b3dd
JeonghoPark <pjhpjh0805@naver.com>
2023-06-06 12:12:26 +0900

    bj_9655

---
+++ src/_23년6월2째주/돌게임_실버5.java
@@ -0,0 +1,20 @@
+package _23년6월2째주;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * https://www.acmicpc.net/problem/9655
+ */
+public class 돌게임_실버5 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        if (n % 2 == 0) {
+            System.out.println("CY");
+        } else {
+            System.out.println("SK");
+        }
+    }
+}
