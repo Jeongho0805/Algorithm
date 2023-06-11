9d90e28fe6a9298fc91d46c74f2d1373da91c74a
JeonghoPark <pjhpjh0805@naver.com>
2023-06-11 12:17:36 +0900

    LeetCode_CoinChange2

---
+++ src/_23년6월2째주/LeetCode_CoinChange2.java
@@ -0,0 +1,14 @@
+package _23년6월2째주;
+
+public class LeetCode_CoinChange2 {
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int coin : coins) {
+            for (int i = coin; i<=amount; i++) {
+                dp[i] += dp[i-coin];
+            }
+        }
+        return dp[amount];
+    }
+}
