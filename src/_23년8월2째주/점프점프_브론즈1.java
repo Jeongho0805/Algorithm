09e793cad7d93331f9dd40909957ffb7e55b26a9
JeonghoPark <pjhpjh0805@naver.com>
2023-08-06 11:47:22 +0900

    bj_18512

---
+++ src/_23년8월2째주/점프점프_브론즈1.java
@@ -0,0 +1,41 @@
+package _23년8월2째주;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+/**
+ * https://www.acmicpc.net/problem/18512
+ * [ 고민 과정 ]
+ * 탐색을 어디까지 해야 될지 정하는게 핵심인것 같은데.. 감을 못잡겠네
+ */
+public class 점프점프_브론즈1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int p1 = Integer.parseInt(st.nextToken());
+        int p2 = Integer.parseInt(st.nextToken());
+
+        HashSet<Integer> set = new HashSet<>();
+        for (int i=0; i<10000/x; i++) {
+            set.add(i*x + p1);
+        }
+
+        int answer = -1;
+        for (int i=0; i<10000/y; i++) {
+            int nextP = p2 + y*i;
+            if (set.contains(nextP)) {
+                answer = nextP;
+                break;
+            }
+        }
+
+        System.out.println(answer);
+
+
+    }
+}
